a25a52e4b1590fa5382985bbd6b39a9bdf92222a
pr1gram <natchathornmoopimon@gmail.com>
2024-09-02 00:18:24 +0700

    update: auth js implement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
-export default function Home() {
+import { signIn, signOut, auth } from "@/api/auth";
+import Image from "next/image";
+
+export default async function Home() {
+  const session = await auth();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main>
+      <form
+        action={async () => {
+          "use server";
+          await signIn("google");
+        }}
+      >
+        <button type="submit">Sign in</button>
+      </form>
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <button type="submit">Sign out</button>
+        <div className=" block">
+          {session?.user?.email}
+          {session?.user?.name}
+          <Image src={session?.user?.image ?? ""} alt="" width={200} height={200} />
+        </div>
+      </form>
     </main>
   );
 }
