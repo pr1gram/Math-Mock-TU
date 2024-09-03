16fb500289f3faa5b518a3c8371839c310601607
Sodynoizz_TH <chonlasitapilardmongkol@gmail.com>
2024-09-04 01:06:13 +0700

    update: reformat code

--- src/app/components/api.tsx
+++ src/app/components/api.tsx
@@ -1,20 +1,20 @@
-import { useEffect } from "react";
-import axios from "axios";
+import { useEffect } from 'react'
+import axios from 'axios'
 
 export default async function apiFunction(method: string, url: string, body: any) {
-  const options = {
-    method: method,
-    url: `${process.env.BASE_URL}${url}`,
-    headers: { "Content-Type": "application/json" },
-    data: body,
-  };
+	const options = {
+		method: method,
+		url: `${process.env.BASE_URL}${url}`,
+		headers: { 'Content-Type': 'application/json' },
+		data: body,
+	}
 
-  try {
-    const response = await axios.request(options);
-    console.log(response.data);
-    return response.data;
-  } catch (error: any) {
-    console.error(error.status);
-    return error.status;
-  }
+	try {
+		const response = await axios.request(options)
+		console.log(response.data)
+		return response.data
+	} catch (error: any) {
+		console.error(error.status)
+		return error.status
+	}
 }
