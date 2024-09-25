1a8ccd1a7972b15aaa2e394cf6c6d0cbf12dff8b
Sodynoizz_TH <chonlasitapilardmongkol@gmail.com>
2024-09-26 01:56:53 +0700

    update: indentation

--- src/app/components/auth/checkSignIn.tsx
+++ src/app/components/auth/checkSignIn.tsx
@@ -3,16 +3,11 @@ import { redirect } from 'next/navigation'
 import { auth } from "@/api/auth";
 
 export default async function  CheckSignIn( isSignedIn:boolean, path:string ) {
-		
-			const session = await auth() // Client-side session fetch
-			if (isSignedIn && session) {
-				redirect(path)
-			} else if (!isSignedIn && !session) {
-				redirect(path)
-			}
-		
-
-		
-
+	const session = await auth() // Client-side session fetch
+	if (isSignedIn && session) {
+		redirect(path)
+	} else if (!isSignedIn && !session) {
+		redirect(path)
+	}
   return null // Return null to prevent rendering anything
 }
