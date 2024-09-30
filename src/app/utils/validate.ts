06a06ece2110247c5ae415d0bb2f63381893bb0b
Adisuarn <adisuarn.w@gmail.com>
2024-10-01 03:04:25 +0700

    test 10

--- src/app/utils/validate.ts
+++ src/app/utils/validate.ts
@@ -9,7 +9,7 @@ export function _validateEmail(email: string) {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
 }
 
-export function verifyEnvironmentKey({ headers }: { headers: Record<string, string | undefined | null> }): any {
+export function verifyEnvironmentKey({ headers }: { headers: any }): any {
   if(!headers) return { success: false, message: "Headers not found", headers }
   if(!headers['x-api-key']) return { success: false, message: "API key not found" , headers}
   if(!process.env.NEXT_PUBLIC_API_KEY) return { success: false, message: "ENV key not found", headers }
