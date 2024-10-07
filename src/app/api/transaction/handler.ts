37e885cc9397896f7234d03b23a976c83601cdd1
Sodynoizz_TH <chonlasitapilardmongkol@gmail.com>
2024-10-08 02:55:29 +0700

    update: map data

--- src/app/api/transaction/handler.ts
+++ src/app/api/transaction/handler.ts
@@ -1,15 +1,9 @@
 ﻿import { firestore } from "@/db/firebase"
 
-import { doc, getDocs ,updateDoc, arrayUnion, query, collection, where } from "firebase/firestore"
+import { doc, getDocs, updateDoc, arrayUnion, query, collection, where } from "firebase/firestore"
 import { error } from "elysia"
+import { getDocumentByEmail, getDocumentById, getSnapshotByQuery, validateEmail } from "@/utils/__init__"
 
-
-import {
-  getDocumentByEmail,
-  validateEmail,
-  getSnapshotByQuery,
-  getDocumentById,
-} from "@/utils/__init__"
 import {
   Status,
   createTransaction,
@@ -19,11 +13,38 @@ import {
 } from "./__init__"
 import type { Slip } from "./__init__"
 
+function getApprovedTestIDs(transactions: any[]): string[] {
+  return transactions
+    .filter(transaction => transaction.status === "approved")
+    .map(transaction => transaction.testID);
+}
+
+async function getExamData(testIDs: string[]): Promise<any[]> {
+  const examData = [];
+
+  for (const testID of testIDs) {
+    const examSnap = await getDocumentById("examLists", testID);
+    if (examSnap?.exists()) {
+      examData.push(examSnap.data());
+    }
+  }
+
+  return examData;
+}
+
+
 export async function transaction(body: Slip) {
   if (!validateEmail(body.email))
     return { success: false, message: "Email is not formatted correctly" }
-  const isTestExist = await getDocs(query(collection(firestore, 'examLists'), where('title', '==', body.testID)))
-  if(isTestExist.empty) return { success: false, message: `Test with testID ${body.testID} does not exist`, status: 404 }
+  const isTestExist = await getDocs(
+    query(collection(firestore, "examLists"), where("title", "==", body.testID))
+  )
+  if (isTestExist.empty)
+    return {
+      success: false,
+      message: `Test with testID ${body.testID} does not exist`,
+      status: 404,
+    }
   const downloadURL = await uploadFile(body.email, body.testID!, body.file)
 
   if (!downloadURL) return { success: false, message: "Cannot get Image URL" }
@@ -49,12 +70,19 @@ export async function transaction(body: Slip) {
 }
 
 export async function userTransactions(email: string) {
-  if (!validateEmail(email)) return { success: false, message: "Email is not formatted correctly" }
+  if (!validateEmail(email)) {
+    return { success: false, message: "Email is not formatted correctly" };
+  }
 
-  const querySnapshot = await getSnapshotByQuery("transactions", "email", email)
-  if (querySnapshot.empty) return { success: false, status: 404, message: "Cannot find user" }
+  const querySnapshot = await getSnapshotByQuery("transactions", "email", email);
+  if (querySnapshot.empty)
+    return { success: false, status: 404, message: "Cannot find user" };
 
-  return { success: true, data: querySnapshot.docs[0].data() }
+  const transactions = querySnapshot.docs[0].data().transactions;
+  const approvedTestIDs = getApprovedTestIDs(transactions);
+
+  const examData = await getExamData(approvedTestIDs);
+  return { success: true, data: querySnapshot.docs[0].data(), examData };
 }
 
 export async function getTransaction(email: string, testID: string) {
@@ -91,11 +119,15 @@ export async function updateStatus(email: string, testID: string, status: Status
       const transactions: Slip[] = docSnap.data().transactions
       const transactionIndex = dfsTransaction(transactions, testID)
       if (transactionIndex === -1)
-        return { success: false, status: 404, message: `Cannot find testId: ${testID} from ${email}` }
+        return {
+          success: false,
+          status: 404,
+          message: `Cannot find testId: ${testID} from ${email}`,
+        }
 
       transactions[transactionIndex].status = status
       await updateDoc(docSnap.ref, { transactions })
-      if ( transactions[transactionIndex].status === "approved" && userSnap?.exists()) {
+      if (transactions[transactionIndex].status === "approved" && userSnap?.exists()) {
         await updateDoc(userSnap.ref, { tests: arrayUnion(testID) })
         return { success: true, message: "Updated Approved Successfully" }
       }
@@ -107,4 +139,3 @@ export async function updateStatus(email: string, testID: string, status: Status
     throw new Error("Error while updating status")
   }
 }
-
