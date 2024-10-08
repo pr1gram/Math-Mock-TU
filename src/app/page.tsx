460e0adde878b70de60422a77f409be9ec7ed402
pr1gram <natchathornmoopimon@gmail.com>
2024-10-09 02:43:12 +0700

    update: error handler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
   if (session) {
     myExamLists = await apiFunction("GET", `/transaction/${session.user?.email}`, {})
   }
-  const myExamListsJSON = JSON.stringify(myExamLists.data.data)
+  const myExamListsJSON = JSON.stringify(myExamLists?.data?.data) || null
 
   return (
     <main className="w-screen overflow-hidden">
