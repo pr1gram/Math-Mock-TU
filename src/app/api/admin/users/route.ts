d04351c99289f100a5b8a7eafae221429bc6af06
Sodynoizz_TH <chonlasitapilardmongkol@gmail.com>
2025-01-01 14:08:16 +0700

    update: rename document

---
+++ src/app/api/admin/users/route.ts
@@ -0,0 +1,25 @@
+import { Elysia, error } from "elysia"
+import { cors } from "@elysiajs/cors"
+import { GlobalGuard } from "@/utils/__init__"
+import { TransactionDocumentValidator } from "../admin.dto"
+import { renameTransactionDocument } from "../admin.controller"
+
+const TransactionRoute = new Elysia({ prefix: "/api/admin" })
+  .use(GlobalGuard)
+  .use(cors({ 
+    origin: ['https://math-mock-tu.vercel.app', 'https://www.pretest-tu.com'] 
+  }))
+  .post(
+    "/",
+    async ({ body }) => {
+      const res = await renameTransactionDocument(body)
+      if (res.success) return res
+      if (res.status === 404) return error(404, `Error: ${res.message}`)
+      else return error(400, `Error: ${res.message}`)
+    },
+    {
+      body: TransactionDocumentValidator,
+    }
+  )
+
+export const POST = TransactionRoute.handle
