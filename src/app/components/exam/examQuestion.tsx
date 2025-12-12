bade9ac980c7bdcc734a9d194d609d1234b260b6
pr1gram <natchathornmoopimon@gmail.com>
2025-12-12 21:33:14 +0700

    update4 december exam

--- src/app/components/exam/examQuestion.tsx
+++ src/app/components/exam/examQuestion.tsx
@@ -10,7 +10,7 @@ const ExamQuestion: React.FC<ExamQuestionProps> = ({ examName, pointNumber }) =>
   return (
     <div className="w-[290px] h-[290px] sm:w-[320px] sm:h-[320px] md:w-[370px] md:h-[370px] lg:w-[500px] lg:min-h-[500px] flex items-center justify-center rounded-[18px] border-2 border-[#b5b6c2] overflow-hidden">
       <Image
-        src={`/data/exam/${examName}/${pointNumber}.jpg`}
+        src={`/data/exam/${examName}/${pointNumber}.png`}
         alt="Exam Image"
         width={700}
         height={700}
